535fd3ae819b0e003ab6a16c9950807cc7c79d98
Tatyana-Kozlikina <kozlikina.t.c@gmail.com>
2023-05-10 16:41:40 +0700

    Исправлено условие определения длины строк

--- src/main/java/ru/example/first_project/MyCompareTo.java
+++ src/main/java/ru/example/first_project/MyCompareTo.java
@@ -7,27 +7,24 @@ public class MyCompareTo {
         int len1=str1.length();
         int len2=str2.length();
         int len=Math.min(len1,len2);
-        if (len>0) {
-            for (i = 0; i <= len; i = i + 1) {
-                char ch1 = str1.charAt(i);
-                char ch2 = str2.charAt(i);
-                if (ch1 == ch2) {
-                    i++;
-                } else if (ch1 != ch2) {
-                    int delta_i = ch1 - ch2;
-                    answer = delta_i;
-                    break;
+        for (i = 0; i <= len; i = i + 1) {
+            char ch1 = str1.charAt(i);
+            char ch2 = str2.charAt(i);
+            if (ch1 == ch2) {
+                i++;
+            }
+            else if (ch1 != ch2) {
+                int delta_i = ch1 - ch2;
+                answer = delta_i;
+                break;
                 }
             }
-        }
-        else {
+        if (answer==0) {
             int delta_len=len1-len2;
             if (delta_len>0) {
-                System.out.println("1");
                 answer=1;
             }
             else if (delta_len<0) {
-                System.out.println("-1");
                 answer=-1;
             }
         }
